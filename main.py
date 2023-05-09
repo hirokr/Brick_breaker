c5d84d4b86a972be2ac875207e6cc708a8532978
hirokr <hirokr@users.noreply.github.com>
2023-05-10 00:23:45 +0600

    initial screen built

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 # Version 1
 import pygame as py 
 from sys import exit
-import math
 
-WINS = [800,800]
+WINS = [800,600]
 
 py.init()
-screen = py.display.set_mode(WINS)
+screen = py.display.set_mode(WINS, py.RESIZABLE)
 font = py.font.Font(None, 50)
 py.display.set_caption('This Brick Breaker By HRR')
-clock = py.time.Clock
+Clock = py.time.Clock()
 screen.fill((111,169,196))
+
 
 class Screen:
     def __init__(self) -> None:
-        pass
+        self.ball = py.draw.circle(surface=screen, color='black',center=(100,100),radius=10)
+        self.brick = py.draw.line(screen, 'yellow', (500,200), (650,200), 30)
         
-
 class Engine(Screen):
     def __init__(self) -> None:
         super().__init__()
-        self.game = True
+        self.game = True 
     
     def play(self):
         while self.game:
             for event in py.event.get():
                 if event.type == py.QUIT:
                     self.game = False
+                if event.type == py.VIDEORESIZE:
+                    self.new_size = event.size 
+                    screen = py.display.set_mode(self.new_size, py.RESIZABLE)
+                    screen.fill('red')
+                    a = Screen()
 
-            py.display.flip()
-            clock.tick(60)
+            py.display.update()
+            Clock.tick(60)
 
-if __name__ == 'main':
-    a = Engine()
-    a.play()
+
+a = Engine()
+a.play()
--- trail.py
+++ trail.py
@@ -1,11 +1,46 @@
-import pygame  
+# import pygame  
   
-pygame.init()  
-screen = pygame.display.set_mode((400,500))  
-done = False  
-  
-while not done:  
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
-            done = True  
-    pygame.display.flip()  
+# pygame.init()  
+# screen = pygame.display.set_mode((400,500))  
+# screen.fill('red')
+# done = False  
+
+# display_info = pygame.display.Info()
+# width = display_info.current_w
+# height = display_info.current_h
+
+# print(width, height)
+
+# def name():  
+#     global done 
+#     while not done:  
+#         for event in pygame.event.get():  
+#             if event.type == pygame.QUIT:  
+#                 done = True  
+#         pygame.display.flip()  
+
+# a = name()
+
+import pygame
+
+pygame.init()
+
+# Set up the initial screen size
+size = (800, 600)
+screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.VIDEORESIZE:
+            # Update the screen size to match the new window size
+            size = event.size
+            screen = pygame.display.set_mode(size, pygame.RESIZABLE)
+
+    # Draw on the screen here
+
+    pygame.display.update()
+
+pygame.quit()
